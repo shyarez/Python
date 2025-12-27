fd8ce0594cd358dcf8d5aa8491b4f8dfe1a82439
Shreya <mohantashreya09@gmail.com>
2025-12-27 21:39:09 +0530

    Delete user_input directory

--- user_input/ShoppingCart.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter your name: ")
-item = input("Select an item to BUY: ")
-price = float(input("The price is: ₹"))
-quantity = int(input("Number of items you would like to buy: "))
-total = price * quantity
-
-print(f"You bought {quantity} x {item}/s")
-print(f"TOTAL BILL: ₹{total}")
